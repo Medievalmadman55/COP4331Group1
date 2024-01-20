b630fcf84d47a5ee70b56e17332a49ad1abec4e3
Medievalmadman55 <chase.hanson9@gmail.com>
2024-01-20 13:27:30 +0000

    initial check in of front end

---
+++ html/LAMPAPI/Registration.php
@@ -0,0 +1,41 @@
+<?php
+$inData = getRequestInfo();
+
+$userId = $inData["userId"];
+$firstname = $inData["firstname"];
+$lastname = $inData["lastname"];
+$username = $inData["username"];
+$password = $inData["password"];
+
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+if ($conn->connect_error) 
+{
+    returnWithError( $conn->connect_error );
+} 
+else
+{
+    $stmt = $conn->prepare("INSERT into Users (UserId,firstname,lastname,username,password) VALUES(?,?,?,?,?)");
+    $stmt->bind_param("sssss", $userId, $firstname,$lastname,$username,$password);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    returnWithError("");
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError( $err )
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
+?>
